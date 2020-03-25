7fb3a5ee312821755f3dc07426b8f99ea2a18d78
Piotr Kurylak <piotrkurylak@gmail.com>
2020-03-25 18:58:02 +0100

    First project initial

---
+++ index.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    const playSound = (e) => {
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+        if (!audio) return; // stop the function from running
+        audio.currentTime = 0;
+        audio.play();
+        key.classList.add('playing');
+
+        const removeTransition = (e) => {
+            if (e.propertyName !== 'transform') return; // skip it if it's not a transform
+            key.classList.remove('playing');
+        }
+
+        const keys = document.querySelectorAll('.key');
+        keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
+    }
+
+
+    window.addEventListener('keydown', playSound);
+
+
+
+
+
+});
